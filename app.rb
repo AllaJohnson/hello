388cb0dad3abb89816f39f8b4c92801e99ba6319
AllaJohnson <deep_ampik@bk.ru>
2014-03-20 23:39:19 +0400

    massiv add in get-block

--- app.rb
+++ app.rb
@@ -1,20 +1,15 @@
 # encoding: utf-8
 require 'sinatra'
 
-@products = [ "prod1",  "prod2",  "prod3",  "prod4",  "prod5" ]
-
 get '/' do  
   erb :'index.html'
 end  
-
 get '/about' do 
    erb :'about.html'
 end  
-
 get '/contacts' do  
    erb :'contacts.html'  
 end
-
-get '/products' do 	
+get '/products' do @products = [ "prod1",  "prod2",  "prod3",  "prod4",  "prod5" ]
   erb :'products.html'
 end
