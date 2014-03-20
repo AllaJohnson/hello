91da691a759d738ffb62aff666ea52b521379420
AllaJohnson <deep_ampik@bk.ru>
2014-03-20 15:36:36 +0400

    massiv add

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 # encoding: utf-8
 require 'sinatra'
-  get '/' do
+
+  @products = [ "prod1",  "prod2",  "prod3",  "prod4",  "prod5" ];
+
+
+  get '/' do  
     erb :'index.html'
   end  
 
-  get '/about' do
+  get '/about' do 
      erb :'about.html'
-     
   end  
 
-  get '/contacts' do
+  get '/contacts' do  
      erb :'contacts.html'  
   end
+  
+  get '/products' do 	
+    erb :'products.html'
+	end
