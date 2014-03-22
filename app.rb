f645e8aa0ec8739e30cf73304ff66f877dd9fb67
AllaJohnson <deep_ampik@bk.ru>
2014-03-22 17:37:44 +0400

    @@hit_counts mod

--- app.rb
+++ app.rb
@@ -1,14 +1,23 @@
 # encoding: utf-8
 require 'sinatra'
-get '/' do  
+@@hit_counts = 0
+def hit
+@@hit_counts += 1
+end
+get '/' do
+  hit
   erb :'index.html'
 end  
 get '/about' do 
-   erb :'about.html'
+  hit
+  erb :'about.html'
 end  
-get '/contacts' do  
-   erb :'contacts.html'  
+get '/contacts' do 
+  hit 
+  erb :'contacts.html'  
 end
-get '/products' do @products = [ "prod1",  "prod2",  "prod3",  "prod4",  "prod5" ]
+get '/products' do
+  hit
+  @products = [ "prod1",  "prod2",  "prod3",  "prod4",  "prod5" ]
   erb :'products.html'
 end
