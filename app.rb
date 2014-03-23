5599aecd0d9038c86e0f635bb6da40c80974595f
AllaJohnson <deep_ampik@bk.ru>
2014-03-23 19:43:15 +0400

    each is beautifull))

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 def hit
 @@hit_counts += 1
 end
+prod = { "prod1" => "10 $", "prod2" => "13 $", "prod3" => "14 $", "prod4" => "18 $", "prod5" => "20 $" }
 get '/' do
   hit
   erb :'index.html'
@@ -19,5 +20,6 @@ end
 get '/products' do
   hit
   @products = [ "prod1",  "prod2",  "prod3",  "prod4",  "prod5" ]
+  @prods = [ { :name =>  "prod1", :cost => "10$", :kind => "bread"}, { :name =>  "prod2", :cost => "13$", :kind => "butter" }, { :name =>  "prod3", :cost => "1$", :kind => "shugar" }, { :name =>  "prod4", :cost => "3$", :kind => "cake" }, { :name =>  "prod5", :cost => "18$", :kind => "cheese" } ]
   erb :'products.html'
 end
