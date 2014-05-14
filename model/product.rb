8dc69b1383ceb7119658b82f3085e803bbb613e8
AllaJohnson <deep_ampik@bk.ru>
2014-05-15 00:32:41 +0400

    classProducts-Product.new

--- model/product.rb
+++ model/product.rb
@@ -1,12 +1,21 @@
 class Product
   attr_accessor :id, :name, :cost, :category_id
-  
+       
+  # def initialize( my_arg = {} )
+  #        @name = initialize[:name]
+  #        @cost = initialize[:cost]
+  #        @category_id = initialize[:category_id]
+  #        puts "my initialize method  #{my_arg}"
+  #        puts "attr_accessor #{@name},#{@cost},#{@category_id}"
+  #   end
   def initialize( name, cost, category_id )
-        @name = name
-        @cost = cost
-        @category_id = category_id 
-      end
-  
+              @name = name
+              @cost = cost
+              @category_id = category_id 
+              puts "my initialize method  #{name}"
+              puts "attr_accessor #{@name},#{@cost},#{@category_id}"
+  end
+    
   def self.all
     q = 'SELECT products.id AS id, 
                 products.name AS name, 
@@ -21,10 +30,10 @@ class Product
   def self.find(id)
     q = "SELECT * FROM products where id = #{id}"
     result = @@db.execute(q).first 
-    product = Product.new( "name", "cost", "category_id" )
-    product.name = result["name"]
-    product.cost = result["cost"]
-    product.category_id = result["category_id"]
+    # product = Product.new( :name => result["name"], :cost => result["cost"], :category_id => result["category_id"] )
+    product = Product.new( result["name"], result["cost"], result["category_id"] )
+    product.id = result["id"]
+    puts "my_product: #{product.id}, #{product.name}, #{product.cost}, #{product.category_id}"
     product
   end
    # def self.new
