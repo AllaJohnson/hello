15731de5c0687570eb22cf77a1ab3448b1c4f9ff
AllaJohnson <deep_ampik@bk.ru>
2014-05-15 11:56:20 +0400

    classProduct-Product.new2

--- model/product.rb
+++ model/product.rb
@@ -1,20 +1,20 @@
 class Product
   attr_accessor :id, :name, :cost, :category_id
        
-  # def initialize( my_arg = {} )
-  #        @name = initialize[:name]
-  #        @cost = initialize[:cost]
-  #        @category_id = initialize[:category_id]
-  #        puts "my initialize method  #{my_arg}"
-  #        puts "attr_accessor #{@name},#{@cost},#{@category_id}"
+  def initialize( my_arg = {} )
+           @name = my_arg[:name]
+           @cost = my_arg[:cost]
+           @category_id = my_arg[:category_id]
+           puts "my initialize method  #{my_arg}"
+           puts "attr_accessor #{@name},#{@cost},#{@category_id}"
+      end
+  # def initialize( name, cost, category_id )
+  #               @name = name
+  #               @cost = cost
+  #               @category_id = category_id 
+  #               puts "my initialize method's name: #{name}"
+  #               puts "attr_accessor #{@name},#{@cost},#{@category_id}"
   #   end
-  def initialize( name, cost, category_id )
-              @name = name
-              @cost = cost
-              @category_id = category_id 
-              puts "my initialize method  #{name}"
-              puts "attr_accessor #{@name},#{@cost},#{@category_id}"
-  end
     
   def self.all
     q = 'SELECT products.id AS id, 
@@ -30,10 +30,9 @@ class Product
   def self.find(id)
     q = "SELECT * FROM products where id = #{id}"
     result = @@db.execute(q).first 
-    # product = Product.new( :name => result["name"], :cost => result["cost"], :category_id => result["category_id"] )
-    product = Product.new( result["name"], result["cost"], result["category_id"] )
+    product = Product.new( :name => result["name"], :cost => result["cost"], :category_id => result["category_id"] )
+    # product = Product.new( result["name"], result["cost"], result["category_id"] )
     product.id = result["id"]
-    puts "my_product: #{product.id}, #{product.name}, #{product.cost}, #{product.category_id}"
     product
   end
    # def self.new
